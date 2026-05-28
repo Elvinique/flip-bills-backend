93ad4f2bbf9339c64b123ef6cf44c9da11c66093
Emmanuel <emmanueltabah@gmail.com>
2026-05-28 13:03:05 +0100

    fix: gofmt travel.go

--- internal/models/travel.go
+++ internal/models/travel.go
@@ -12,7 +12,7 @@ const (
 	BookingConfirmed   TravelBookingStatus = "confirmed"
 	BookingPending     TravelBookingStatus = "pending"
 	BookingRescheduled TravelBookingStatus = "rescheduled"
-	BookingCancelled TravelBookingStatus = "canceled"
+	BookingCancelled   TravelBookingStatus = "canceled"
 	BookingBoarded     TravelBookingStatus = "boarded"
 )
 
